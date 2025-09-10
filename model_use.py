82c9c94b6f8f41358ff44ce4db15ea1ce7298d56
naomiliu6671 <130464358+naomiliu6671@users.noreply.github.com>
2025-09-10 21:01:10 +0800

    Update model_use.py

--- model_use.py
+++ model_use.py
@@ -1,5 +1,25 @@
-import pandas as pd
+from process.to_fingerprint import molecule_to_fingerprint
 
+def use_model(run_data):
+    smiles_path = '../data/mito_smiles.csv'
+    maccs_path = '../data/mito_maccs.csv'
+    fcfp4_path = '../data/mito_fcfp4.csv'
+    if os.path.exists(smiles_path) == False:
+        message = molecule_to_fingerprint(run_data)
+        print(message)
 
-data = pd.read_csv('data.csv')
+    smiles = pd.read_csv(smiles_path)
+    maccs = pd.read_csv(maccs_path)
+    fcfp4 = pd.read_csv(fcfp4_path)
+
+    data = pd.concat([maccs, fcfp4], axis=1)
+    data.columns = [i for i in range(167+2048)]
+
+    model = joblib.load('../mito_toxicity_model.joblib')
+
+    tox = model.predict(data)
+    mito_tox = smiles
+    mito_tox['mito_tox'] = tox
+    mito_tox.to_csv('../data/mito_tox_pred.csv', index=False)
+    return mito_tox
 
